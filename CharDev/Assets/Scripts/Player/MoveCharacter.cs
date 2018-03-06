f5dee75bb1d72f99157048d0bb1ce12f7a4e4556
AnnaEdmondson <puckpranks.cluefinder@gmail.com>
2018-03-06 09:09:17 -0700

    Copied Project from broken Repository

---
+++ CharDev/Assets/Scripts/Player/MoveCharacter.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class MoveCharacter : MonoBehaviour {
+
+	CharacterController cc;
+	public PlayerData data;
+
+	Vector3 tempMove;
+//	public float speed = 7;
+//	public float gravity = 0.8f;
+//	public float jumpHeight = .2f;						this stuff is in the scriptable object
+//	int jumpCount = 0;
+//	int jumpNumber = 2;
+	public GameObject Player;
+	bool first = true;
+
+	void Start () {
+		cc = GetComponent<CharacterController>();
+		Player.GetComponent<moveInput>().canPlay = true;
+				moveInput.JumpAction = Jump;
+		if(first == true){
+		moveInput.KeyAction += Move;
+		first = false;
+		}
+}
+
+
+	void Jump(){
+		if(data.jumpCount < data.jumpNumber){
+			tempMove.y = data.jumpHeight;
+			data.jumpCount ++;
+		}
+	}
+	
+	void Move (float _movement) {
+		if( !cc.isGrounded)
+		{tempMove.y -= data.gravity*Time.deltaTime;
+		}
+		tempMove.x = _movement*data.speed*Time.deltaTime;
+		cc.Move(tempMove);
+		
+		if(cc.isGrounded){
+			data.jumpCount = 0;
+		}
+	}
+	
+
+}
---
+++ CharDev/Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PlayerData")]
+
+public class PlayerData : ScriptableObject {
+
+	public float speed;
+	public float gravity;
+	public float jumpHeight;
+	public int jumpCount;
+	public int jumpNumber;
+
+
+}
